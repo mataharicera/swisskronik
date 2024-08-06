a44f91763112b8124c519aba8ac09c9974197e16
ClackSon <119122298+mataharicera@users.noreply.github.com>
2024-08-06 16:53:51 +0000

    feat: initiated the project

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("swis20","sisw20"){} 
+    constructor()ERC20("monye","popo"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
